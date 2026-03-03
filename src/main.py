642d281c16cc4887af37a22688a18a2349d6c152
Bakir Yunusov <yunusovbekir@gmail.com>
2026-03-04 00:56:31 +0100

    update vout

--- src/main.py
+++ src/main.py
@@ -44,7 +44,7 @@ def play():
         "--no-mouse-events",
         "--no-keyboard-events",
         "--no-osd",
-        "--vout", "drm",        # force DRM directly
+        "--vout", "mmal",    # Raspberry Pi vout
         "--no-audio",
     )
 
