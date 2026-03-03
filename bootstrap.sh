5643eaf201d3aa104061bd3131a0e25d97fbfe6e
Bakir Yunusov <yunusovbekir@gmail.com>
2026-03-04 00:24:35 +0100

    add pip install to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -76,8 +76,8 @@ fi
 # --- Install dependencies -----------------------------------------------------
 echo "[1/3] Installing dependencies (git, vlc, wlr-randr, raindrop, cec-utils)..."
 apt update
-apt install -y git vlc libvlc-dev wlr-randr raindrop cec-utils pipx
-pipx install python-vlc
+apt install -y git vlc libvlc-dev wlr-randr raindrop cec-utils
+pip install python-vlc --break-system-packages 
 echo "[$(date -Iseconds)] [1/3] Dependencies: OK"
 
 # --- Clone or update repo -----------------------------------------------------
